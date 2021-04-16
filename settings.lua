4014dd4f07ce656b554b81cb06ab7f4488694507
ZwerOxotnik <zweroxotnik@gmail.com>
2021-04-16 15:05:27 +0300

    Update to v2.1.1

--- settings.lua
+++ settings.lua
@@ -1,18 +1,1 @@
-local SWITCHABLE_COMMANDS = require("mod-commands").get_commands()
-
--- Don't use symbols like '-' etc (it'll break pattern of regular expressions)
-local MOD_SHORT_NAME = "zo_teams_"
-
-local commands = {}
-for key, command in pairs(SWITCHABLE_COMMANDS) do
-	local command_name = command.name or key
-	commands[#commands + 1] = {
-		type = "bool-setting",
-		name = MOD_SHORT_NAME .. key,
-		setting_type = "runtime-global",
-		default_value = command.default_value or true,
-		localised_name = '/' .. command_name,
-		localised_description = command.description or ""
-	}
-end
-data:extend(commands)
+require("mod-commands").set_settings()
